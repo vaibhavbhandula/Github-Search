69a1ca8aae758cc0f1e97d7cfa85fb87d6ec2168
Vaibhav Bhandula <vaibhavbhandula@gmail.com>
2017-02-23 18:00:04 +0530

    intial

---
+++ app/src/main/java/com/test/githubsearch/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.test.githubsearch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.githubsearch.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
